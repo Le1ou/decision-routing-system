626e649a0a508f236741cdcf7b53c806c7492d43
Zlodemon <95135474+Zlodemon@users.noreply.github.com>
2026-05-14 17:46:48 +0300

    feat: add simple code to work with DB and images
    -Added functions to encode/decode base64
    -Added multiple functions to write rows into a db
    -Added simple config file for changable params
    -Added image of my cat

---
+++ apps/backend/src/application_module.py
@@ -0,0 +1,107 @@
+import psycopg
+import  psycopg_pool
+import atexit
+import json
+from pathlib import Path
+from datetime import datetime, timezone
+import base64
+configPath = Path(__file__).parent.parent / "config.json"
+global configData
+project_timezone = timezone.utc
+with configPath.open() as config_data:
+    configData = json.load(config_data)
+    config_data.close()
+
+class PgDbOperator:
+    delegated_to_same_dep = configData["dep_configs"]["delegated_to_same_dep"]
+    empl_appl_delay = configData["dep_configs"]["empl_appl_delay"]
+    deadline_notification = configData["dep_configs"]["deadline_notification"]
+    def __init__(self):
+        conn_info = "dbname=app_db user=postgres password=postgres"
+        self.pool = psycopg_pool.ConnectionPool(conninfo=conn_info, min_size=1, max_size= 10)
+        atexit.register(self.pool.close)
+        self.pool.wait()
+        print("connection pool ready")
+
+    def writeNewPost(self, name = "Отдел", is_top = False):
+         with self.pool.connection() as conn:
+            conn.execute('INSERT INTO post (name, is_top' \
+                                                        ") VALUES (%s,%s)", (name, is_top))
+            
+    def writeNewDepartment(self, name = "Отдел", group = "Основной", value = 0,  delegated_to_same_dep = delegated_to_same_dep, empl_appl_delay = empl_appl_delay, deadline_notification = deadline_notification):
+         with self.pool.connection() as conn:
+            conn.execute('INSERT INTO department ("group", value, name, ' \
+                                                        "delegated_to_same_dep, empl_appl_delay, " \
+                                                        "deadline_notification) VALUES (%s,%s,%s,%s,%s,%s)", (group, value, name, delegated_to_same_dep, empl_appl_delay, deadline_notification))
+            
+    def tryWriteNewEmployee(self, fio = "Имя сотрудника", department_id = -1, post_id = -1):
+        now = datetime.now(project_timezone)
+        with self.pool.connection() as conn:
+            try:
+                conn.execute('INSERT INTO employee (post_id, department_id, fio, ' \
+                                                        "created_at, updated_at " \
+                                                        ") VALUES (%s,%s,%s,%s,%s)", (post_id, department_id, fio,now ,now ))
+            except psycopg.errors.ForeignKeyViolation:
+                conn.rollback()
+                print("Error: Foreign key value does not exist.")
+
+    def tryWriteNewTypeOfWork(self, name = "Вид работы", department_id = -1, complexity_value = 0):
+        with self.pool.connection() as conn:
+            try:
+                conn.execute('INSERT INTO types_of_work (name, complexity_value, department_id, ' \
+                                                        ") VALUES (%s,%s,%s)", (name, complexity_value , department_id))
+            except psycopg.errors.ForeignKeyViolation:
+                conn.rollback()
+                print("Error: Foreign key value does not exist.")
+
+    def writeNewComplexityValue(self, name = "Сложность"):
+         with self.pool.connection() as conn:
+            conn.execute('INSERT INTO complexity_value (name' \
+                                                        ") VALUES (%s)", (name))
+    def writeNewStatus(self, name = "Состояние"):
+         with self.pool.connection() as conn:
+            conn.execute('INSERT INTO status (name' \
+                                                        ") VALUES (%s)", (name))
+    def writeNewRole(self, name = "Состояние"):
+         with self.pool.connection() as conn:
+            conn.execute('INSERT INTO role (name' \
+                                                        ") VALUES (%s)", (name))
+    def writeNewGrade(self, name = "Ранг"):
+         with self.pool.connection() as conn:
+            conn.execute('INSERT INTO grade (name' \
+                                                        ") VALUES (%s)", (name))
+    def writeNewPriority(self, name = "Значение приоритета", value = 0):
+         with self.pool.connection() as conn:
+            conn.execute('INSERT INTO priority (name, value' \
+                                                        ") VALUES (%s, %s)", (name, value))
+    ### Осталось - фото, many-to-many таблицы, уведомления, !!!! Заявка!!!!.     
+
+
+
+def convertPhotoToBase64(photo):
+    try:
+        with open(Path(__file__).parent.parent / photo, "rb") as image_file:
+            encoded_string = base64.b64encode(image_file.read()).decode('utf-8')
+            return encoded_string
+    except:
+        print("cant convert photo")
+
+def convertBase64ToPhoto(bytePhoto, shouldWriteToFile = False, WriteToDirectory = (Path(__file__).parent.parent), NameToWrite = "output_image.png"):
+    try:
+        image_data = base64.b64decode(bytePhoto)
+        if shouldWriteToFile:
+            with open(WriteToDirectory/NameToWrite, "wb") as f:
+                f.write(image_data)
+        return image_data
+    except:
+        print("cant convert photo from this string")
+
+#print(convertPhotoToBase64("test_img.jpg"))
+#print(convertBase64ToPhoto(convertPhotoToBase64("test_img.jpg")))
+
+
+#test = PgDbOperator()
+#test.writeNewDepartment("Отдел безопасности", "основное отделение", 1, True, 10)
+#test.writeNewPost(name = "Сотрудник", is_top = False)
+#test.tryWriteNewEmployee("Иванов Иван Иванович", 1, 1)
+#test.tryWriteNewTypeOfWork("Починить комп", 1, 1)
